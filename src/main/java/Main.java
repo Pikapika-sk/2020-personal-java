74fdde70d5e745d1ab9203dac8547fb09200591d
Sikun Hong <784308941@qq.com>
2020-09-09 15:40:58 +0800

    Commit message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World " + " Are you ok"+"Are you ok?");
-    }
+import com.alibaba.fastjson.JSON;
+import  com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
+import com.alibaba.fastjson.parser.JSONToken;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+        List<Person> listPersons = new ArrayList<>();
+        listPersons.add(new Person(15, "John Doe", new Date()));
+        listPersons.add(new Person(25, "xqq", new Date()));
+        String s = JSON.toJSONString(listPersons);
+        System.out.println(s);
+    }
 }
+
---
+++ src/main/java/Person.java
@@ -0,0 +1,45 @@
+import com.alibaba.fastjson.annotation.JSONField;
+
+import java.util.Date;
+
+public class Person {
+    @JSONField(name = "AGE")
+    private int age;
+
+    @JSONField(name = "FULL NAME")
+    private String fullName;
+
+    @JSONField(name = "DATE OF BIRTH")
+    private Date dateOfBirth ;
+
+    public Person(int age, String fullName, Date dateOfBirth) {
+        super();
+        this.age = age;
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
