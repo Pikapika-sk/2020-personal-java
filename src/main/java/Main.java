04419af334b4f8c7edb4f43e286d81720d926eae
Sikun Hong <784308941@qq.com>
2020-09-13 23:17:57 +0800

    Commit message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -53,6 +53,7 @@ public class Main {
     public static void main(String[] args) {
 
         String fileName = ".\\src\\test.json";
+
         ArrayList<String> jsonList = readJsonFile(fileName);
         Map<String, Result> map1 = new HashMap<>(); //个人 4 种事件的数量。
         Map<String, Result> map2 = new HashMap<>();//每一个项目的 4 种事件的数量。
