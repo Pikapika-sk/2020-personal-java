08357ab7ff8d3c50a3a0c3cfbc5d86aaed3b3128
Sikun Hong <784308941@qq.com>
2020-09-15 19:56:46 +0800

    Commit message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import com.alibaba.fastjson.*;
+import org.apache.commons.io.FileUtils;
+
+import javax.lang.model.element.NestingKind;
+import javax.print.DocFlavor;
 
 //还未处理的：init后的Map如何存储成文件判断时再读入
 // 类型3判断有错，感觉是/的原因
@@ -12,7 +16,8 @@ public class Main {
     static Map<String, Result> map1 = new HashMap<>(); //个人 4 种事件的数量。
     static Map<String, Result> map2 = new HashMap<>();//每一个项目的 4 种事件的数量。
     static Map<String, Result> map3 = new HashMap<>();//每一个人在每一个项目的 4 种事件的数量。
-    private static void init(String fileName) {
+
+    private static void init(String fileName) throws IOException {
         ArrayList<String> jsonList = new ArrayList<>();
         File dir = new File(fileName);
         File[] files = dir.listFiles(file -> file.getName().endsWith(".json"));
@@ -32,12 +37,12 @@ public class Main {
             e.printStackTrace();
         }
         for (int i = 0; i < jsonList.size(); i++) {
-          //  System.out.println("i = "+ i);
+            //  System.out.println("i = "+ i);
             String tmp = jsonList.get(i);
             //     System.out.println(i);
             //   System.out.println("tmp = "+ tmp);
             tmp = justify(tmp);
-         //   System.out.println(tmp);
+            //   System.out.println(tmp);
             JSONObject jso1 = JSON.parseObject(tmp);
             String actor = jso1.getString("actor");
             JSONObject jso2 = JSON.parseObject(actor);
@@ -47,8 +52,8 @@ public class Main {
             JSONObject jso3 = JSON.parseObject(repo);
             String repoId = jso3.getString("name");
 
-            String idAndRepoId =   id +   repoId;
-        //    System.out.println(idAndRepoId);
+            String idAndRepoId = id + repoId;
+            //    System.out.println(idAndRepoId);
 
             //  System.out.println(repoId);
             if (!map1.containsKey(id)) {
@@ -92,9 +97,19 @@ public class Main {
             map3.put(idAndRepoId, iAndRepoRes);
 
         }
-//        System.out.println(map1.size());
-//        System.out.println(map2.size());
-//        System.out.println(map3.size());
+        //   System.out.println(map3.keySet().size());
+
+
+        String s1 = JSONObject.toJSONString(map1);;
+        String s2 = JSONObject.toJSONString(map2);
+        String s3 = JSONObject.toJSONString(map3);
+        // 将json写入文件
+
+    //   System.out.println(s1);
+        FileUtils.writeStringToFile(new File("map1.json"), s1, "UTF-8");
+        FileUtils.writeStringToFile(new File("map2.json"), s2, "UTF-8");
+        FileUtils.writeStringToFile(new File("map3.json"), s3, "UTF-8");
+
     }
 
     private static String justify(String s) {
@@ -115,41 +130,65 @@ public class Main {
         return ans;
     }
 
-    public static int output(Result r, String event)
-    {
-        int ans=0;
-        System.out.println("event = "+ event);
-        if(event.equals("PushEvent")){
-            ans= r.PushEvent;
-        }else if(event.equals("IssueCommentEvent")){
+    public static int output(Result r, String event) {
+        int ans = 0;
+        System.out.println("event = " + event);
+        if (event.equals("PushEvent")) {
+            ans = r.PushEvent;
+        } else if (event.equals("IssueCommentEvent")) {
             ans = r.IssueCommentEvent;
-        }else if(event.equals("PullRequestEvent")){
+        } else if (event.equals("PullRequestEvent")) {
             ans = r.PullRequestEvent;
-        }else if(event.equals("IssuesEvent")){
+        } else if (event.equals("IssuesEvent")) {
             ans = r.IssuesEvent;
         }
-     //   System.out.println("ans = "+ ans);
+        //   System.out.println("ans = "+ ans);
         return ans;
     }
 
-    public static int countFromUser(String user,String event){
-        Result r = map1.get(user);
-        return output(r,event);
+    public static void countFromUser(String user, String event) throws IOException {
+        String tmp = event.substring(0,1);
+        tmp = tmp.toLowerCase();
+        event = tmp +event.substring(1,event.length());
+        String s = FileUtils.readFileToString(new File("map1.json"), "UTF-8");
+    //   System.out.println(s);
+   //     System.out.println("user = "+ user + "event = " + event);
+        JSONObject jsonObject = JSONObject.parseObject(s);
+  //      System.out.println(jsonObject);
+        JSONObject object = jsonObject.getJSONObject(user);
+     //   System.out.println(object);
+   //     System.out.println("result = " +result);
+       if(object !=null){
+           System.out.println( object.getInteger(event));
+       }
 
     }
 
-    public static int countFromRepo(String repo,String event){
+    public static void countFromRepo(String repo, String event) throws IOException {
+        String tmp = event.substring(0,1);
+        tmp = tmp.toLowerCase();
+        event = tmp +event.substring(1,event.length());
+        String s = FileUtils.readFileToString(new File("map2.json"), "UTF-8");
+        JSONObject jsonObject = JSONObject.parseObject(s);
+        JSONObject object = jsonObject.getJSONObject(repo);
+        System.out.println(object.getInteger(event));
+    }
 
-        Result r = map2.get(repo);
-        return output(r,event);
+    public static void countFromUserAndRepo(String idAndRepoId, String event) throws IOException {
+        String tmp = event.substring(0,1);
+        tmp = tmp.toLowerCase();
+        event = tmp +event.substring(1,event.length());
+        String s = FileUtils.readFileToString(new File("map3.json"), "UTF-8");
+        JSONObject jsonObject = JSONObject.parseObject(s);
+        JSONObject object = jsonObject.getJSONObject(idAndRepoId);
+        if (object != null) {
+            System.out.println( object.getInteger(event));
+        }
     }
-    public static int countFromUserAndRepo(String idAndRepoId,String event){
-        Result r = map3.get(idAndRepoId);
-        return output(r,event);
-    }
-    public static void main(String[] args) {
-       // init("C:\\Users\\78430\\Desktop\\test\\json");
-        init("C:\\Users\\78430\\Desktop\\test\\json");
+
+    public static void main(String[] args) throws IOException {
+        // init("C:\\Users\\78430\\Desktop\\test\\json");
+      //  init("C:\\Users\\78430\\Desktop\\test\\json");
 //
 //        args[0] = "-u";
 //        args[1] ="cdupuis";
@@ -159,35 +198,40 @@ public class Main {
 //        args[5] ="PushEvent";
 
 
-        ArrayList<String> jsonList=null;
-        String testUser=null,testEvent=null,testRepo=null,filename =null;
+
+        ArrayList<String> jsonList = null;
+        String testUser = null, testEvent = null, testRepo = null, filename = null;
 //        for(String arg : args){
 //            System.out.println("arg =" +arg);
 //        }
-  //      System.out.println(map1.size()+" "+ map2.size()+map3.size());
-        if(args.length !=0) {
+        //      System.out.println(map1.size()+" "+ map2.size()+map3.size());
+        if (args.length != 0) {
             if (args[0].equals("--init") || args[0].equals("-i ")) {
                 filename = args[1];
                 init(filename);
             } else if (args[0].equals("-u") || args[0].equals("--user")) {
-                testUser = args[1];
-                if (args[2].equals("-e") || args[2].equals("--event")) {
-                    testEvent = args[3];
-                    System.out.println(countFromUser(testUser, testEvent));
-                } else if (args[2].equals("-r") || args[2].equals("repo")) {
-                    testRepo = args[3];
-                    testEvent = args[5];
-                    System.out.println(countFromUserAndRepo(testUser + testRepo, testEvent));
+                if (args.length > 2) {
+                    testUser = args[1];
+                    if (args[2].equals("-e") || args[2].equals("--event")) {
+                        testEvent = args[3];
+                        countFromUser(testUser, testEvent);
+                    } else if (args[2].equals("-r") || args[2].equals("repo")) {
+                        testRepo = args[3];
+                        testEvent = args[5];
+                        countFromUserAndRepo(testUser + testRepo, testEvent);
+                    }
                 }
             } else if (args[0].equals("-r") || args[0].equals("--repo")) {
                 testRepo = args[1];
                 testEvent = args[3];
-                System.out.println(countFromRepo(testRepo, testEvent));
+                countFromRepo(testRepo, testEvent);
             }
         }
-     //   System.out.println(map1.size());
-     //   System.out.println(map2.size());
-      //  System.out.println(map3.size());
+         //  countFromUser("waleko","PushEvent");
+        //java -Dfile.encoding=UTF-8 -jar GHAnalysis.jar -u  cdupuis -e PushEvent
+        //   System.out.println(map1.size());
+        //   System.out.println(map2.size());
+        //  System.out.println(map3.size());
 
     }
 
--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,10 +1,43 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Result {
+public class Result implements Serializable {
     int PushEvent;
     int IssueCommentEvent;
     int IssuesEvent;
     int PullRequestEvent;
+
+    public int getIssueCommentEvent() {
+        return IssueCommentEvent;
+    }
+
+    public int getIssuesEvent() {
+        return IssuesEvent;
+    }
+
+    public int getPushEvent() {
+        return PushEvent;
+    }
+
+    public int getPullRequestEvent() {
+        return PullRequestEvent;
+    }
+
+    public void setIssueCommentEvent(int issueCommentEvent) {
+        IssueCommentEvent = issueCommentEvent;
+    }
+
+    public void setIssuesEvent(int issuesEvent) {
+        IssuesEvent = issuesEvent;
+    }
+
+    public void setPullRequestEvent(int pullRequestEvent) {
+        PullRequestEvent = pullRequestEvent;
+    }
+
+    public void setPushEvent(int pushEvent) {
+        PushEvent = pushEvent;
+    }
 
     @Override
     public String toString() {
